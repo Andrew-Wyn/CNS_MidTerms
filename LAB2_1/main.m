9e6e21d2feb356e20b5f43f9a0d6c25879ad1808
Luca Moroni <terraformer.144@gmail.com>
2023-03-29 18:02:41 +0200

    added LAB2_1 ex

---
+++ LAB2_1/main.m
@@ -0,0 +1,66 @@
+% Hebbian learning
+
+% -- load the dataset
+data = table2array(readtable('lab2_1_data.csv'));
+
+% -- initialize the weight vector of dimension 2
+% --- uniform sampling between [-1, 1]
+w = (2).*rand(2,1) - 1;
+
+ws = w;
+
+epochs = 10;
+
+learning_rate = 0.01;
+thr = 0.1;
+
+for ep = 1:epochs
+    for i = randperm(size(data, 2))
+        % generate v
+        v_i = w'*data(:, i);
+        
+        % compute the hebb step
+        w = w + learning_rate*(v_i*data(:, i));
+        
+        ws = [ws, w];
+    end
+    
+    if norm(w - ws(end-1)) <= thr
+        break
+    end
+end
+
+% -- Plots
+
+% --- P1
+
+% compute the eigenvector of correlation matrix
+[eigs_vects, eigs] = eig(data*data');
+
+[~, argmax] = max(diag(eigs));
+max_eig_vect = eigs_vects(:, argmax);
+
+scatter(data(1, :), data(2, :))
+hold on
+% plot the weight vector
+normalized_w = w/norm(w);
+line([0,normalized_w(1,1)],[0,normalized_w(2,1)], Color="red", LineWidth=1.5)
+hold on
+% plot the eig vector
+line([0,max_eig_vect(1,1)],[0,max_eig_vect(2,1)], Color="green", LineWidth=1.5)
+legend('','weight vector', 'eig vector');
+hold off
+pause();
+
+
+% --- P2
+% plot the first component of the weight vectors in time
+plot(ws(1, :));
+pause();
+
+% plot the second component of the weight vectors in time
+plot(ws(2, :));
+pause();
+
+% plot the norm of the weight vectors in time
+plot(vecnorm(ws));
